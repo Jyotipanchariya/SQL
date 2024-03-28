5121f260f52ebc2153548979095f857bbf757778
Jyoti panchariya <143185652+Jyotipanchariya@users.noreply.github.com>
2024-03-28 11:49:08 +0530

    Add files via upload

---
+++ SQL 9 JOIN.sql
@@ -0,0 +1,27 @@
+use sakila;
+show tables;
+describe actor;
+select * from film_actor;
+desc film_actor;
+
+select * from film_category;
+desc film_category;
+
+
+create table product(pid int, pname varchar(20));
+insert into product values(1,"tv"),(2,"mobile"),(3,"abc");
+
+create table orders(oid int , amount int, pid int);
+insert into orders values(1,100,1),(2,200,3),(3,500,1),(4,800,90);
+select * from product;
+select * from orders;
+
+select oid,amount,orders.pid,pname
+from orders join product
+where (orders.pid=product.pid);
+
+
+-- using  is used when column name is same called inner join
+select oid,amount,orders.pid,pname
+from orders join product
+using (pid);
